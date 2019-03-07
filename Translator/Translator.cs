744cc21249bed502f5380ad8977c0b5da46e45cd
Conor O'Shaughnessy <coshaugh1@gmail.com>
2019-03-07 00:57:25 -0800

    setting up translator project scaffolding

---
+++ Translator/Translator.cs
@@ -0,0 +1,38 @@
+using System.IO;
+
+namespace Translator
+{
+    public class Translator
+    {
+        private Stream inputStream;
+
+        public Translator()
+        {
+            
+        }
+
+        public bool Initialize(Stream input)
+        {
+            if(inputStream != null)
+                return false;
+
+            inputStream = input;
+            return true;
+        }
+
+        public void Reset()
+        {
+            inputStream = null;
+        }
+
+        public Stream GetHeader()
+        {
+            return Stream.Null;
+        }
+
+        public Stream GetSource()
+        {
+            return Stream.Null;
+        }
+    }
+}
