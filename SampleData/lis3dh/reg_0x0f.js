abe291733b5d8effe89549715880f48a016bc8fb
Conor O'Shaughnessy <coshaugh1@gmail.com>
2021-07-09 16:08:23 -0700

    first completed register of new format

---
+++ SampleData/lis3dh/reg_0x0f.js
@@ -0,0 +1,106 @@
+var REG_0x0f = {
+  Name: "WHO_AM_I",
+  Address: 0x0f,
+  Description: "Device identification register",
+  BitGroups: [
+    {
+      Bits: [
+        {
+          Position: 0,
+        },
+      ],
+      States: [
+        {
+          Values: [1],
+        },
+      ],
+    },
+    {
+      Bits: [
+        {
+          Position: 1,
+        },
+      ],
+      States: [
+        {
+          Values: [1],
+        },
+      ],
+    },
+    {
+      Bits: [
+        {
+          Position: 2,
+        },
+      ],
+      States: [
+        {
+          Values: [0],
+        },
+      ],
+    },
+    {
+      Bits: [
+        {
+          Position: 3,
+        },
+      ],
+      States: [
+        {
+          Values: [0],
+        },
+      ],
+    },
+    {
+      Bits: [
+        {
+          Position: 4,
+        },
+      ],
+      States: [
+        {
+          Values: [1],
+        },
+      ],
+    },
+    {
+      Bits: [
+        {
+          Position: 5,
+        },
+      ],
+      States: [
+        {
+          Values: [1],
+        },
+      ],
+    },
+    {
+      Bits: [
+        {
+          Position: 6,
+        },
+      ],
+      States: [
+        {
+          Values: [0],
+        },
+      ],
+    },
+    {
+      Bits: [
+        {
+          Position: 7,
+        },
+      ],
+      States: [
+        {
+          Values: [0],
+        },
+      ],
+    },
+  ],
+  Register_Dependencies: null,
+};
+
+export { REG_0x0f };
